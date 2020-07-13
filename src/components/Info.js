20628f043226b359c2a881377f68c632c1f3dee6
tony <serratedhumor@yahoo.com>
2020-07-13 14:17:19 -0500

    added artist credits

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ImgCredits from './infoComponents/ImgCredits'
 function Info() {
     return (
         <div className='infoCont'>
@@ -52,6 +52,7 @@ function Info() {
             <p>In the interest of usability several assumptions were made about alcohol content and drink sizes. Where possible an additional calculation is made to account for overpours (both the original and adjusted numbers are available on screen), but several more considerations for overpours may need to be made.</p>
             <p>This app is designed to be used to get a better estimation of standard drinks consumed, but it is far from perfect. Nothing about this app should be interpreted to be medical or legal advice.</p>
             <h4>Citations</h4>
+            <ImgCredits />
         </div>
     )
 }
